0d35d774f7db214863cecb56a024e8cd23f588be
Rayyan Naveed <rayyannaveed33@gmail.com>
2025-12-07 12:51:34 +0500

    Update the book

--- book/.docusaurus/client-modules.js
+++ book/.docusaurus/client-modules.js
@@ -1,7 +1,6 @@
 export default [
-  require('F:\\Programs\\nocode_dev\\physical_ai_native_book\\book\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('F:\\Programs\\nocode_dev\\physical_ai_native_book\\book\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('F:\\Programs\\nocode_dev\\physical_ai_native_book\\book\\node_modules\\@docusaurus\\theme-classic\\lib\\admonitions.css'),
-  require('F:\\Programs\\nocode_dev\\physical_ai_native_book\\book\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('F:\\Programs\\nocode_dev\\physical_ai_native_book\\book\\src\\css\\custom.css'),
+  require("F:\\Programs\\nocode_dev\\physical_ai_native_book\\book\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("F:\\Programs\\nocode_dev\\physical_ai_native_book\\book\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("F:\\Programs\\nocode_dev\\physical_ai_native_book\\book\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("F:\\Programs\\nocode_dev\\physical_ai_native_book\\book\\src\\css\\custom.css"),
 ];
